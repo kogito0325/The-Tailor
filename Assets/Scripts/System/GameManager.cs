ef053f14bcbe62338133e13f31b8f6982eb9942d
kogito0325 <qasw405@gmail.com>
2025-08-27 23:08:14 +0900

    0827 - 게임 매니저 임시 추가

---
+++ Assets/Scripts/System/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = Time.timeScale == 1.0f ? 0 : 1.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
